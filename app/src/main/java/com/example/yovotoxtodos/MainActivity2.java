0e2179b8a8b136e0e12b8cb31be7d4aef871530d
Whatare <adriangutierrezmartinez378@gmail.com>
2023-03-15 01:34:09 +0100

    init

--- app/src/main/java/com/example/yovotoxtodos/MainActivity2.java
+++ app/src/main/java/com/example/yovotoxtodos/MainActivity2.java
@@ -21,7 +21,21 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         findViewById(R.id.imageButton).setOnClickListener(v -> navigate(CandidatosData.pinar));
-        findViewById(R.id.imageButton2).setOnClickListener(v -> navigate(CandidatosData.habana));
+        findViewById(R.id.imageButton2).setOnClickListener(v -> navigate(CandidatosData.artemisa));
+        findViewById(R.id.imageButton3).setOnClickListener(v -> navigate(CandidatosData.habana));
+        findViewById(R.id.imageButton4).setOnClickListener(v -> navigate(CandidatosData.mayabeque));
+        findViewById(R.id.imageButton5).setOnClickListener(v -> navigate(CandidatosData.isla));
+        findViewById(R.id.imageButton6).setOnClickListener(v -> navigate(CandidatosData.matanzas));
+        findViewById(R.id.imageButton7).setOnClickListener(v -> navigate(CandidatosData.villa));
+        findViewById(R.id.imageButton8).setOnClickListener(v -> navigate(CandidatosData.cienfuego));
+        findViewById(R.id.imageButton10).setOnClickListener(v -> navigate(CandidatosData.santi));
+        findViewById(R.id.imageButton11).setOnClickListener(v -> navigate(CandidatosData.ciego));
+        findViewById(R.id.imageButton12).setOnClickListener(v -> navigate(CandidatosData.camaguey));
+        findViewById(R.id.imageButton13).setOnClickListener(v -> navigate(CandidatosData.tunas));
+        findViewById(R.id.imageButton14).setOnClickListener(v -> navigate(CandidatosData.holguin));
+        findViewById(R.id.imageButton15).setOnClickListener(v -> navigate(CandidatosData.granma));
+        findViewById(R.id.imageButton16).setOnClickListener(v -> navigate(CandidatosData.santiago));
+        findViewById(R.id.imageButton17).setOnClickListener(v -> navigate(CandidatosData.guantanamo));
 
 
     }
